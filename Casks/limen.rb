a33d8b2a15ae3fb88ff411120a0415a66fed6213
Ranajoy Dutta <ranajoydutta7@gmail.com>
2026-09-09 20:31:38 +0530

    chore: upgrade release actions to node24 and update homebrew tap docs

--- Casks/limen.rb
+++ Casks/limen.rb
@@ -1,13 +1,13 @@
 cask "limen" do
   version "0.1.0"
-  sha256 "REPLACE_WITH_SHA256"
+  sha256 "b3d03019839cd5d9d3da5fae7eb0f27d1ef9c512776b2c0d0b1f8c49604a30fd"
 
   url "https://github.com/ranajoy-dutta/limen/releases/download/v#{version}/Limen_#{version}_aarch64.dmg"
   name "Limen"
   desc "Native macOS menu bar AWS credential switcher for IAM Identity Center"
   homepage "https://github.com/ranajoy-dutta/limen"
 
-  depends_on macos: ">= :monterey"
+  depends_on macos: :monterey
 
   app "Limen.app"
 
